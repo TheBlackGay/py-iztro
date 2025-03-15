14c968246176dda0bb4c32c9ca5f54bdc92dec4d
钟佳峰 <zhongjiafeng@bwton.com>
2025-03-15 16:32:23 +0800

    可以正常跑了！docker也正常！ 不再从网上下载py_iztro

--- api/py-iztro-master/src/tests/daxian.py
+++ api/py-iztro-master/src/tests/daxian.py
@@ -2,10 +2,10 @@ from py_iztro import Astro
 
 
 def main():
-    # 获取命盘
     astro = Astro()
     result = astro.by_solar("2000-8-16", 2, "女")
-    print(result.model_dump_json(by_alias=True, indent=4))
+    result = result.horoscope("2025-01-01").model_dump_json(by_alias=True, indent=4)
+    print(result)
 
 
 if __name__ == "__main__":
